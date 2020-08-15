4f37e7710a465a7cfc51877d9cdc6bd4cfe24f68
mastja <56243990+mastja@users.noreply.github.com>
2020-08-14 20:08:22 -0600

    Update my-workouts.js

--- Website Files JS/my-workouts.js
+++ Website Files JS/my-workouts.js
@@ -2,9 +2,9 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-
+ /*Function getDailydWorkouts returns the following selected workouts*/
     function getDailyWorkouts(res, mysql, context, userID, complete){
-        var sql = "SELECT workoutName, bodyPart, linkToVideo, sets, reps FROM DailyWorkouts WHERE userID=?";
+        var sql = "SELECT Workouts.workoutName, Workouts.bodyPart, Workouts.linkToVideo, UserWorkouts.sets, UserWorkouts.reps FROM UserWorkouts, Workouts WHERE userID=? and UserWorkouts.completed=`N`";
         var inserts = [userID];
         mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
@@ -16,9 +16,9 @@ module.exports = function(){
         });
     }
     
- /*Function getCompletedWorkouts od the following selected workouts*/
+ /*Function getCompletedWorkouts returns the following selected workouts*/
     function getCompletedWorkouts(res, mysql, context, userID, complete){
-        var sql = "SELECT workoutName, bodyPart, sets, reps FROM DailyWorkouts WHERE userID=?";
+        var sql = "SELECT Workouts.workoutName, Workouts.bodyPart, Workouts.linkToVideo, UserWorkouts.sets, UserWorkouts.reps FROM UserWorkouts, Workouts WHERE userID=? and UserWorkouts.completed=`Y`";
         var inserts = [userID];
         mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
@@ -35,6 +35,7 @@ module.exports = function(){
     router.get('/', function(req, res){
     	var callbackCount = 0;
         var context = {};
+        context.jsscripts = ["completeworkout.js"];
         var mysql = req.app.get('mysql');
         getDailyWorkouts(res, mysql, context, req.params.userID, complete);
         getCompletedWorkouts(res, mysql, context, req.params.userID, complete);
@@ -46,4 +47,19 @@ module.exports = function(){
         }
     });
 
-    /*Router URI for "complete a workout" functionality goes here, delete from dailyWorkouts and insert into Completed workouts*/
+    /*Router URI for "complete a workout" functionality goes here, updates completed attribute in workouts to 'y'*/
+
+    router.put('/:userWorkoutID', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "UPDATE UserWorkouts SET completed=`Y` WHERE userWorkoutID=?";
+        var inserts = [req.params.userWorkoutID];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
+            }
+        });
+    });
